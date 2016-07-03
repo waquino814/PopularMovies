5562aa3b41da2f451ce338189218e828bdb7b702
waquino814 <wellingtonaquino@Wellingtons-MacBook-Pro.local>
2016-07-03 11:18:02 -0400

    setting new main activity with on fragment

--- app/src/main/java/org/wellingtonaquino/popularmovies/MainActivity.java
+++ app/src/main/java/org/wellingtonaquino/popularmovies/MainActivity.java
@@ -2,7 +2,6 @@ package org.wellingtonaquino.popularmovies;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
 
     }
 
